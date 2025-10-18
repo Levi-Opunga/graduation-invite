93a0d4fc2a5f05e236004328a606aae0c5336d67
Levi-Opunga <leviopunga@gmail.com>
2025-10-18 11:43:56 +0300

    add pre tags to message

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,23 @@ export default async function RootLayout({
 
     return (
         <html lang="en">
+        <head>
+            <meta property="og:type" content="website"/>
+            <meta property="og:title" content="Levi’s Graduation Party 🎓"/>
+            <meta property="og:image" content="https://grad.codekxlabs.com/uploads/squares.png"/>
+            <meta property="og:description" content="Join me in celebrating this special milestone! Click to RSVP."/>
+            <meta property="og:url" content="https://grad.codekxlabs.com/"/>
+            <meta property="og:site_name" content="Levi’s Graduation Party 🎓"/>
+            <meta property="og:locale" content="en_US"/>
+            <meta property="og:image:width" content="1200"/>
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:title" content="Levi’s Graduation Party 🎓"/>
+            <meta name="twitter:description" content="Join me in celebrating this special milestone! Click to RSVP."/>
+            <meta name="twitter:image" content="https://grad.codekxlabs.com/uploads/squares.png"/>
+            <meta name="twitter:site" content="@codekxlabs"/>
+            <meta name="twitter:creator" content="@codekxlabs"/>
+            <meta name="twitter:url" content="https://grad.codekxlabs.com/"/>
+        </head>
         <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} ${playfair.variable}`}>
         <Suspense fallback={<LoaderPage/>}>{children}</Suspense>
         <Analytics/>
